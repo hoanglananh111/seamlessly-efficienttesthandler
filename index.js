e4e3fd5183a4090f15541d83c20620e25f596d57
hoanglananh111 <tchadaunitpun0suy@hotmail.com>
2024-05-09 01:23:54 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const titleCase = (str) =>
-  str.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
+function buildTree(inorder, postorder) {
+  if (!inorder.length || !postorder.length) return null;
+  const rootVal = postorder[postorder.length - 1];
+  const root = new TreeNode(rootVal);
+  const mid = inorder.indexOf(rootVal);
+  root.left = buildTree(inorder.slice(0, mid), postorder.slice(0, mid));
+  root.right = buildTree(
+    inorder.slice(mid + 1),
+    postorder.slice(mid, postorder.length - 1),
+  );
+  return root;
+}
