909883bb028ae2137e9a41c16f79737d5c556af2
hoanglananh111 <tchadaunitpun0suy@hotmail.com>
2024-05-10 05:42:04 +0700

    start commit

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-function buildTree(inorder, postorder) {
-  if (!inorder.length || !postorder.length) return null;
-  const rootVal = postorder[postorder.length - 1];
-  const root = new TreeNode(rootVal);
-  const mid = inorder.indexOf(rootVal);
-  root.left = buildTree(inorder.slice(0, mid), postorder.slice(0, mid));
-  root.right = buildTree(
-    inorder.slice(mid + 1),
-    postorder.slice(mid, postorder.length - 1),
-  );
-  return root;
-}
+const dropWhile = (arr, func) => {
+  while (arr.length > 0 && !func(arr[0])) arr = arr.slice(1);
+  return arr;
+};
