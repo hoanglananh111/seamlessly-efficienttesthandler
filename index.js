79739b04f494ba6abbc3d41094bafdf48ecf12f8
hoanglananh111 <tchadaunitpun0suy@hotmail.com>
2024-05-13 16:13:37 +0700

    add more features

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const lcmRecursive = (a, b) => {
-  return (a * b) / gcdRecursive(a, b);
-};
+function isSubsequence(s, t) {
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) i++;
+    j++;
+  }
+  return i === s.length;
+}
